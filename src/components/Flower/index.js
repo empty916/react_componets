65172b52e54d0f2de43e1a6aec7c70d3c01a99b4
ex-wangxiaogang002 <ex-wangxiaogang002@pingan.com.cn>
2018-02-06 11:37:18 +0800

    将菊花样式变成可配置

--- src/components/Flower/index.js
+++ src/components/Flower/index.js
@@ -1,20 +1,131 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import './style.scss'
 
 /**
  * 这个组件只是一个菊花🌻
  * petal：花瓣
  */
-const petalNum = 24;
-let petals = [];
-for(let i=0; i < petalNum; i++) {
-    petals.push(<div key={i} className='petal'/>);
-}
+const r = px => {
+    if(px=='1'){
+        return '1px';
+    } else if(/%/.test(px)){
+        return px;
+    } else {
+        return px / 75 + 'rem';
+    }
+}; // 样式单位适配
 
-const Flower = () => (
-    <div id="flower">
-        <div className="wrapper">{petals}</div>
-    </div>
-);
+class Flower extends PureComponent{
+
+    constructor(){
+        super();
+        const petalNum = 24; // 花瓣数量
+        const time = 0.05; // 一个菊花闪烁的频率，数值越小表示 菊花转的越快 
+        const petalWidth = 4; // 花瓣的宽度
+        const petalHeight = 18; // 花瓣的高度
+        const petalBorderRadius = 1;  // 花瓣的圆角
+        const petalBackGroundColor = '#000';
+        const initOpacity = '0.05'; // 菊花 初始|最小 透明度
+        const animationDuration = time * petalNum; // 菊花转一圈的时间
+        const rotateDeg = 360/petalNum; // 每一瓣花瓣的旋转角度
+
+        this.state = {
+            petalNum,
+            time,
+            petalWidth,
+            petalHeight,
+            petalBorderRadius,
+            petalBackGroundColor,
+            initOpacity,
+            animationDuration,
+            rotateDeg,
+        };
+    }
+    componentWillMount() {
+        this.refresh();
+    }
+    componentWillReceiveProps(newProps){
+        this.refresh(newProps);
+    }
+    refresh = props => {
+        let { 
+            size, 
+            time,
+            bgColor, 
+            petalNum,
+            petalWidth, 
+            petalHeight, 
+            petalBorderRadius } = props ? props : this.props;
+        time = time ? time : this.state.time;
+
+        let state = {};
+        state.time = time;
+        state.petalHeight = !!petalHeight ? petalHeight : (size / 4);
+        state.petalBorderRadius = petalBorderRadius;
+        state.petalBackGroundColor = bgColor;
+        state.petalWidth = petalWidth;
+        state.petalNum = petalNum;
+        state.animationDuration = state.time * petalNum; // 菊花转一圈的时间
+        state.rotateDeg = 360/petalNum; // 每一瓣花瓣的旋转角度
+        this.setState(state);
+    };
+    flowerStyle = () => (
+        { // 花的样式
+            width: r(this.state.petalHeight * 4),
+            height: r(this.state.petalHeight * 4),
+        }
+    );
+    getPetalStyle = (index) => {
+        const {
+            petalWidth,
+            petalHeight,
+            initOpacity,
+            petalBackGroundColor,
+            petalBorderRadius,
+            animationDuration,
+            rotateDeg,
+            time,
+        } = this.state;
+
+        
+        return { // 根据index生成petal样式
+            width: r(petalWidth),
+            height: r(petalHeight),
+            position: 'absolute',
+            left: '50%',
+            opacity: initOpacity,
+            backgroundColor: petalBackGroundColor,
+            borderRadius: r(petalBorderRadius),
+            transformOrigin: `0 ${r(petalHeight * 2)}`,
+            transform: `rotate(${index * rotateDeg}deg)`,
+            webkitAnimation: `load ${animationDuration}s ease infinite`,
+            animation: `load ${animationDuration}s ease infinite`,
+            animationDelay: `${time * index}s`,
+        }
+    };
+
+    
+    render(){
+        let petals = [];
+        for(let i=0; i < this.state.petalNum; i++) {
+            petals.push(<div  style = {this.getPetalStyle(i)}/>);
+        }
+
+        return(
+            <div id="flower" style={this.flowerStyle()}>
+                <div className="wrapper">{petals}</div>
+            </div>
+        )
+    }
+}
+Flower.defaultProps = {
+    size: 72, // 菊花大小 优先级低于petaHeight
+    petalWidth: 4, // 花瓣宽度
+    // petalHeight: 18, // 花瓣高度
+    petalBorderRadius: 1, // 花瓣圆角大小
+    bgColor: '#fff', // 花瓣背景色
+    petalNum: 24, // 花瓣数量
+    // time: 0.05*24, // 转动一圈所用的时间
+};
 
 export default Flower;
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,14 +3,27 @@ import Flower from '../Flower'
 import './style.scss'
 
 class Loading extends PureComponent{
+    state = {
+        petalNum: 12,
+        petalWidth: 3,
+        petalHeight: 10,
+        time: 0.09,
+        petalBorderRadius: '50%',
+    };
     render(){
+        const size = parseInt(Math.random()*100);
+        const state = {
+            petalWidth: size,
+            petalHeight: size,
 
+        };
         return(
             <div id="loading">
                 <div className="wrapper">
-                    <Flower/>
+                    <Flower {...this.state}/>
                     <div className="loading-text">{this.props.text}</div>
                 </div>
+                <button style={{width: '200px',height: '60px', position: 'absolute', bottom: '0'}} onClick={() => {this.setState(state)}}>change</button>
             </div>
         )
     }
