4113dbad457865dd1a115d5e2754ccb46920b503
ex-wangxiaogang002 <ex-wangxiaogang002@pingan.com.cn>
2018-02-12 10:30:31 +0800

    完善测试案例

---
+++ src/App/index.js
@@ -0,0 +1,45 @@
+import React, { PureComponent } from 'react'
+import './style.scss'
+import Img from '../components/Img'
+import imgs from '../data/img'
+
+const btnStyle = {width: '100%',height: '200px',backgroundColor:'lightblue'};
+
+class App extends PureComponent {
+    state = {
+        imgNum: 1,
+        showImg: false,
+    };
+    changeImgNum = (e) => {
+        this.setState({
+            imgNum: e.target.value,
+        });
+    };
+    render() {
+        const {
+            imgNum,
+            showImg,
+        } = this.state;
+
+        const sources = imgs.slice(0, Math.min(imgNum, 2000)).map((item, index)=><Img className='img' index={index} src={item} lazy/>);
+        return (
+            <div id='app'>
+                {showImg?sources:[
+                    <input
+                        type="number"
+                        max={2000}
+                        min={0}
+                        placeholder={'最大2000'}
+                        onChange={this.changeImgNum}/>
+                    ,<button style={btnStyle} onClick={() => this.setState({
+                        showImg: true,
+                    })}>确定</button>
+                ]}
+            </div>
+        );
+    }
+}
+
+App.propTypes = {};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './style/core/base.scss'
-import './style.scss'
-import Img from './components/Img'
-import Slide from './components/Slide'
-import imgs from './data/img'
+import App from './App'
 
 const rootElement = document.getElementById('app');
-
-const sources = imgs.slice(0, 2000).map((item, index)=><Img className='img' index={index} src={item} lazy/>);
-console.log(imgs.length);
 ReactDOM.render(
-	<div>
-        {/*<Slide sources={sources}/>*/}
-        {sources}
-    </div>,
+    <App/>,
     rootElement
 );
