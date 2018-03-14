3e08f3b8f0aba83353f20e10b3a1672b459bba27
empty916 <empty916@qq.com>
2018-03-14 20:54:12 +0800

    add img

--- src/components/BgImg/index.js
+++ src/components/BgImg/index.js
@@ -17,15 +17,15 @@ let bgiS = {
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'auto 100%',
     backgroundPosition: 'center',
-    backgroundImage: `url(${require('../../images/background/bai_he.jpeg')})`,
+    backgroundImage: `url(${require('../../images/background/shan.jpg')})`,
     justifyContent: 'center',
     alignItems: 'center',
 };
 
 let content = {
-    marginTop: `${-600/75}rem`,
-    // marginRight: `${500/75}rem`,
-    fontSize: `${130/75}rem`,
+    marginTop: `${-700/75}rem`,
+    marginRight: `${450/75}rem`,
+    fontSize: `${80/75}rem`,
     fontFamily: fontMap['mnfz'],
     // textShadow: '1px 1px 0px #212121',
     // color: '#030303',
@@ -35,8 +35,8 @@ let content = {
 class BgImg extends PureComponent{
     state = {
         isFullScreen: false,
-        col1Text: '慎 独',
-        col2Text: '对境忘情',
+        col1Text: '望去千重山',
+        col2Text: '高深渐可攀',
         optionHide: false,
     };
     constructor(params) {
@@ -73,7 +73,7 @@ class BgImg extends PureComponent{
                     <div className='qk-font'
                          ref='content'
                          style={content} >
-                        {/*{spaces}{col2Text}<br/>*/}
+                        {spaces}{col2Text}<br/>
                         {col1Text}
                     </div>
                 </div>
