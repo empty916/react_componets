d1efd771f19a6e670c5d764cd3b49647a623b542
ex-wangxiaogang002 <ex-wangxiaogang002@pingan.com.cn>
2018-03-15 10:11:11 +0800

    加图

--- src/components/BgImg/index.js
+++ src/components/BgImg/index.js
@@ -12,30 +12,33 @@ let fontMap = {
 };
 let spaces = <span>&nbsp;&nbsp;</span>;
 
+const rem = num => `${num/75}rem`;
+
 let bgiS = {
     background: `no-repeat center 100% auto`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'auto 100%',
     backgroundPosition: 'center',
-    backgroundImage: `url(${require('../../images/background/shan.jpg')})`,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundImage: `url(${require('../../images/background/chuan.jpg')})`,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
 };
 
 let content = {
-    marginTop: `${-700/75}rem`,
-    marginRight: `${450/75}rem`,
-    fontSize: `${80/75}rem`,
+    // marginTop: `${rem(-700)}`,
+    // marginRight: `${rem(450)}`,
+    margin: `0 ${rem(10)} ${rem(155)} 0`,
+    fontSize: `${rem(80)}`,
     fontFamily: fontMap['mnfz'],
     // textShadow: '1px 1px 0px #212121',
-    // color: '#030303',
+    color: '#adadad',
     // marginBottom: '20px',
 };
 
 class BgImg extends PureComponent{
     state = {
         isFullScreen: false,
-        col1Text: '望去千重山',
+        col1Text: '临 江 仙',
         col2Text: '高深渐可攀',
         optionHide: false,
     };
@@ -68,12 +71,12 @@ class BgImg extends PureComponent{
         return(
             <div id='bg-img' 
                  ref='wrapper'
-                 onClick={this.toggleFullScreen}
+                 // onClick={this.toggleFullScreen}
                  style={bgiS}>
                     <div className='qk-font'
                          ref='content'
                          style={content} >
-                        {spaces}{col2Text}<br/>
+                        {/*{spaces}{col2Text}<br/>*/}
                         {col1Text}
                     </div>
                 </div>
